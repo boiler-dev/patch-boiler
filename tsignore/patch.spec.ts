468fe0272ff36dcda71b451d28b27d851bb43761
winton <mail@wintoni.us>
2020-03-03 13:22:01 -0800

    Add describe to test

--- tsignore/patch.spec.ts
+++ tsignore/patch.spec.ts
@@ -17,64 +17,66 @@ class A {
 
 const a = new A()
 
-it("patches", () => {
-  expect.assertions(9)
+describe("patch", () => {
+  it("patches", () => {
+    expect.assertions(9)
 
-  let calls = []
+    let calls = []
 
-  patch.create(
-    a,
-    "patchMe",
-    {
-      fn: (hi: string) => {
-        expect(hi).toBe("hi")
-        calls.push(-1)
+    patch.create(
+      a,
+      "patchMe",
+      {
+        fn: (hi: string) => {
+          expect(hi).toBe("hi")
+          calls.push(-1)
+        },
+        order: -1,
       },
-      order: -1,
-    },
-    {
-      patchMe: a.patchMe.bind(a),
-      order: 0,
-    },
-    {
-      fn: (hi: string) => {
-        expect(hi).toBe("hi")
-        calls.push(1)
+      {
+        patchMe: a.patchMe.bind(a),
+        order: 0,
       },
-    }
-  )
+      {
+        fn: (hi: string) => {
+          expect(hi).toBe("hi")
+          calls.push(1)
+        },
+      }
+    )
 
-  expect(a.patchMe("hi")).toBe(true)
-  expect(calls).toEqual([-1, 1])
+    expect(a.patchMe("hi")).toBe(true)
+    expect(calls).toEqual([-1, 1])
 
-  calls = []
+    calls = []
 
-  patch.update(
-    a.patchMe,
-    {
-      fn: (hi: string) => calls.push(-2),
-      order: -2,
-    },
-    {
-      after: (hi: string) => {
-        calls.push(2)
-        return 2
+    patch.update(
+      a.patchMe,
+      {
+        fn: (hi: string) => calls.push(-2),
+        order: -2,
       },
-      order: 2,
-      return: true,
-    }
-  )
+      {
+        after: (hi: string) => {
+          calls.push(2)
+          return 2
+        },
+        order: 2,
+        return: true,
+      }
+    )
 
-  expect(a.patchMe("hi")).toBe(2)
-  expect(calls).toEqual([-2, -1, 1, 2])
+    expect(a.patchMe("hi")).toBe(2)
+    expect(calls).toEqual([-2, -1, 1, 2])
 
-  expect(patch.find(a.patchMe).memo).toEqual({
-    fn: undefined,
-    patchMe: true,
-    after: 2,
+    expect(patch.find(a.patchMe).memo).toEqual({
+      fn: undefined,
+      patchMe: true,
+      after: 2,
+    })
   })
-})
 
-it("resets", () => {
-  expect(a.patchMe("")).toBe(true)
+  it("resets", () => {
+    expect(a.patchMe("")).toBe(true)
+  })
 })

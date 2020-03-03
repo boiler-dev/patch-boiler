c342299baddfb86b032b8e560a212bcd09ba5220
winton <mail@wintoni.us>
2020-03-03 12:47:34 -0800

    Generator

---
+++ tsignore/patch.spec.ts
@@ -0,0 +1,78 @@
+import expect from "./expect"
+
+import loaded from "./loaded"
+import tinyId from "./tiny-id"
+import patch from "../src/patch"
+
+loaded.load({ patch, tinyId })
+beforeEach(() => patch.reset())
+
+class A {
+  patchMe(hi: string): boolean {
+    return true
+  }
+}
+
+const a = new A()
+
+it("patches", () => {
+  expect.assertions(9)
+
+  let calls = []
+
+  patch.create(
+    a,
+    "patchMe",
+    {
+      fn: (hi: string) => {
+        expect(hi).toBe("hi")
+        calls.push(-1)
+      },
+      order: -1,
+    },
+    {
+      patchMe: a.patchMe.bind(a),
+      order: 0,
+    },
+    {
+      fn: (hi: string) => {
+        expect(hi).toBe("hi")
+        calls.push(1)
+      },
+    }
+  )
+
+  expect(a.patchMe("hi")).toBe(true)
+  expect(calls).toEqual([-1, 1])
+
+  calls = []
+
+  patch.update(
+    a.patchMe,
+    {
+      fn: (hi: string) => calls.push(-2),
+      order: -2,
+    },
+    {
+      after: (hi: string) => {
+        calls.push(2)
+        return 2
+      },
+      order: 2,
+      return: true,
+    }
+  )
+
+  expect(a.patchMe("hi")).toBe(2)
+  expect(calls).toEqual([-2, -1, 1, 2])
+
+  expect(patch.find(a.patchMe).memo).toEqual({
+    fn: undefined,
+    patchMe: true,
+    after: 2,
+  })
+})
+
+it("resets", () => {
+  expect(a.patchMe("")).toBe(true)
+})
---
+++ tsignore/patch.ts
@@ -0,0 +1,187 @@
+import { Fn2 } from "./loaded"
+import tinyId from "./tinyId"
+
+interface Patches {
+  id: string
+  fn: Function
+  memo: Record<string, any>
+}
+
+interface Originals {
+  instance: any
+  fnName: string
+  fn: Function
+}
+
+export class Patch {
+  fn2: Fn2 = null
+  tinyId: typeof tinyId = null
+
+  originals: Originals[]
+  patches: Patches[]
+  steps: Record<string, Record<string, any>[]>
+
+  constructor() {
+    this.reset()
+  }
+
+  create(
+    instance: any,
+    fnName: string,
+    ...steps: Record<string, any>[]
+  ): Record<string, any> {
+    const id = this.tinyId.generate()
+    const memo = this.update(id, ...steps)
+
+    const fn = (...args: any[]): Promise<any> | any => {
+      const out = this.fn2.run(
+        memo,
+        args,
+        ...this.steps[id]
+      )
+      const returnFnId = this.returnFnId(id)
+
+      if (out.then) {
+        return out.then(
+          (out: Record<string, any>) => out[returnFnId]
+        )
+      } else {
+        return out[returnFnId]
+      }
+    }
+
+    this.originals = this.originals.concat({
+      fn: instance[fnName],
+      fnName,
+      instance,
+    })
+
+    instance[fnName] = fn
+
+    this.patches = this.patches.concat({
+      id,
+      fn,
+      memo,
+    })
+
+    return memo
+  }
+
+  update(
+    id: string,
+    ...steps: Record<string, any>[]
+  ): Record<string, any>
+
+  update(
+    fn: Function,
+    ...steps: Record<string, any>[]
+  ): Record<string, any>
+
+  update(
+    idOrFn: string | Function,
+    ...steps: Record<string, any>[]
+  ): Record<string, any> {
+    let id: string
+    let fn: Function
+    let memo: Record<string, any>
+
+    if (typeof idOrFn === "string") {
+      id = idOrFn
+      memo = {}
+    } else {
+      ;({ id, fn, memo } = this.find(idOrFn))
+      this.remove(idOrFn)
+    }
+
+    this.steps[id] = this.steps[id] || []
+    this.steps[id] = this.steps[id]
+      .concat(this.fillSteps(steps))
+      .sort(this.sortSteps)
+
+    if (fn) {
+      this.patches = this.patches.concat({ id, fn, memo })
+    }
+
+    return memo
+  }
+
+  find(id: string): Patches
+  find(fn: Function): Patches
+  find(fnOrString: Function | string): Patches {
+    return this.patches.find(({ id, fn }) => {
+      return typeof fnOrString === "string"
+        ? fnOrString === id
+        : fnOrString === fn
+    })
+  }
+
+  remove(fn: Function): void {
+    this.patches = this.patches.filter(
+      ({ fn: f }) => fn !== f
+    )
+  }
+
+  reset(): void {
+    if (this.originals) {
+      for (const { instance, fnName, fn } of this
+        .originals) {
+        instance[fnName] = fn
+      }
+    }
+
+    this.originals = []
+    this.patches = []
+    this.steps = {}
+  }
+
+  private fillSteps(
+    steps: Record<string, any>[]
+  ): Record<string, any>[] {
+    let order = 1
+
+    for (const step of steps) {
+      if (step.order !== undefined) {
+        order = step.order
+      } else {
+        order = order + 1
+        if (order === 0) {
+          order += 1
+        }
+        step.order = order
+      }
+    }
+
+    return steps
+  }
+
+  private returnFnId(id: string): string {
+    const mainFn = this.steps[id]
+      .concat([])
+      .reverse()
+      .find(
+        step => step.order === 0 || step.return === true
+      )
+
+    if (!mainFn) {
+      throw new Error(
+        "Provide a return step with { order: 0 } or { return: true }."
+      )
+    }
+
+    return Object.keys(mainFn).find(
+      key =>
+        ["args", "order", "returnArgs", "return"].indexOf(
+          key
+        ) < 0
+    )
+  }
+
+  private sortSteps(
+    { order: a }: Record<string, any>,
+    { order: b }: Record<string, any>
+  ): number {
+    return a > b ? 1 : a < b ? -1 : 0
+  }
+}
+
+export default new Patch()
